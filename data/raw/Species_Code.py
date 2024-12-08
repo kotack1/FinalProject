a54234ce0c10702a392ee0001aa84f8ba1f24bc4
Tani-ValdezRivas <tani.valdezrivas@duke.edu>
2024-12-08 12:24:54 -0500

    Changes to Species_Code

--- data/raw/Species_Code.py
+++ data/raw/Species_Code.py
@@ -22,25 +22,21 @@ common_names = "common_name"
 warm_months_df = pd.read_csv(warm_months)
 cold_months_df = pd.read_csv(cold_months)
 
+print("Warm Months Columns:", warm_months_df.columns.tolist())
+print("Cold Months Columns:", cold_months_df.columns.tolist())
+
 # Get unique species and counts
 warm_months_counts = warm_months_df[common_names].value_counts().reset_index()
 warm_months_counts.columns = ["Species", "Warm_Month_Count"]
 cold_months_counts = cold_months_df[common_names].value_counts().reset_index()
 cold_months_counts.columns = ["Species", "Cold_Month_Count"]
 
-# Dispaly names and counts
-#warm_months_counts.index.name = "Common Name - Warm"
-#print(warm_months_counts)
-
-#cold_months_counts.index.name = "Common Name - Cold "
-#print(cold_months_counts)
-
 # Merge counts on species name
-merged_counts = pd.merge(warm_months_counts, cold_months_counts, on = "Common_Name",how = "outer").fillna(0)
+merged_counts = pd.merge(warm_months_counts, cold_months_counts, on="Species", how="outer").fillna(0)
 
 #Covert counts to integers
-merged_counts["Warm_Count"] = merged_counts["Warm_Count"].astype(int)
-merged_counts["Cold_Count"] = merged_counts["Cold_Count"].astype(int)
+merged_counts["Warm_Month_Count"] = merged_counts["Warm_Month_Count"].astype(int)
+merged_counts["Cold_Month_Count"] = merged_counts["Cold_Month_Count"].astype(int)
 
 # Save to CSV file
 unique_counts = f"V:/FinalProject/data/processed/Species_Counts.csv"
